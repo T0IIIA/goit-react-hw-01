391a665e438c6c041f49990394031a67a3bcdabd
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-07-02 01:09:18 +0300

    start hw

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import s from "./App.module.css";
+
+const App = () => {
+  return <h1 className={s.hello}>HELLO</h1>;
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
